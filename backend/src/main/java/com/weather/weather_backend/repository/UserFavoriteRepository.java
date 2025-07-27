93a8a96b779841754a654fb6c490157001d1084e
NisalUthsara <nisaluthsara1@gmail.com>
2025-07-27 15:21:46 +0530

    minor update - repo

--- backend/src/main/java/com/weather/weather_backend/repository/UserFavoriteRepository.java
+++ backend/src/main/java/com/weather/weather_backend/repository/UserFavoriteRepository.java
@@ -2,12 +2,12 @@ package com.weather.weather_backend.repository;
 
 import com.weather.weather_backend.model.UserFavorite;
 import com.weather.weather_backend.model.UserFavoriteId;
-import org.springframework.data.convert.ReadingConverter;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@ReadingConverter
+@Repository
 public interface UserFavoriteRepository extends JpaRepository<UserFavorite, UserFavoriteId> {
     List<UserFavorite> findByUserId(Long userId);
 }
